f7e25549e9c2594b1dc891d8ca87b7290c3f78f4
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-25 16:16:52 -0300

    Add users context module

---
+++ lib/bank_ex/contexts/users.ex
@@ -0,0 +1,41 @@
+defmodule BankEx.Contexts.Users do
+  @moduledoc """
+  The User's context.
+  """
+  import Ecto.Query
+
+  alias BankEx.Repo
+  alias BankEx.Schemas.User
+
+  @preload [:referral_user]
+
+  @doc """
+
+  """
+  @spec get(user_id :: Ecto.UUID.t()) :: {:ok, User.t()} | {:error, term()}
+  def get(user_id) do
+    from(
+      m in User,
+      where: m.id == ^user_id,
+      preload: ^@preload
+    )
+    |> Repo.one()
+    |> case do
+      nil ->
+        {:error, :not_found}
+
+      user ->
+        {:ok, user}
+    end
+  end
+
+  @doc """
+  Create new user
+  """
+  @spec create(map()) :: {:ok, User.t()} | {:error, term()}
+  def create(attrs) when is_map(attrs) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+end
