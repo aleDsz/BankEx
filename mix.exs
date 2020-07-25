241923572c72d4a578cf529f379ac1b6f0cadb39
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-25 12:54:04 -0300

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -1,61 +1,57 @@
 defmodule BankEx.MixProject do
   use Mix.Project
 
-  def project do
-    [
-      app: :bank_ex,
-      version: "0.1.0",
-      elixir: "~> 1.7",
-      elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
-      start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
-      deps: deps()
-    ]
-  end
+  def project, do: [
+    app: :bank_ex,
+    version: "0.1.0",
+    elixir: "~> 1.8",
+    elixirc_paths: elixirc_paths(Mix.env()),
+    compilers: [:phoenix] ++ Mix.compilers(),
+    start_permanent: Mix.env() == :prod,
+    aliases: aliases(),
+    deps: deps()
+  ]
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
-  def application do
-    [
-      mod: {BankEx.Application, []},
-      extra_applications: [:logger, :runtime_tools]
-    ]
-  end
+  def application, do: [
+    mod: {BankEx.Application, []},
+    extra_applications: [:logger, :runtime_tools]
+  ]
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # API
       {:phoenix, "~> 1.5.3"},
       {:phoenix_ecto, "~> 4.1"},
+      {:plug_cowboy, "~> 2.0"},
+
+      # Database
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
+      
+      # Telemetry
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
+
+      # Serializers
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+
+      # Documentation
+      {:phoenix_swagger, github: "xerions/phoenix_swagger", branch: "master"},
+
+      # Testing
+      {:ex_machina, "~> 2.4", only: [:dev, :test]},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      setup: ["ecto.drop --quiet", "ecto.create --quiet", "ecto.migrate --quiet"],
+      swagger: ["phx.swagger.generate ./priv/static/swagger.json --router BankExWeb.Router --endpoint BankExWeb.Endpoint"]
     ]
   end
 end
