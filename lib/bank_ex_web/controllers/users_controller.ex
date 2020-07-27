4ce628a39b71e2188f0f59089f3fa02a2027ecf8
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-26 22:41:09 -0300

    Improve function to create new user using the User service

--- lib/bank_ex_web/controllers/users_controller.ex
+++ lib/bank_ex_web/controllers/users_controller.ex
@@ -6,12 +6,19 @@ defmodule BankExWeb.UsersController do
 
   action_fallback BankExWeb.FallbackController
 
-  alias BankEx.Contexts.Users
+  alias BankEx.Services.Users
 
   @doc """
   Create new user
   """
   @spec create(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def create(%Plug.Conn{} = conn, %{"referral_code" => referral_code} = params) when is_map(params) do
+    with {:ok, referral_user} <- Users.get_by_referral_code(referral_code),
+         params <- Map.put(params, "referred_user_id", referral_user.id),
+         {:ok, user} <- Users.create(params) do
+      conn |> put_status(201) |> render("user.json", user: user)
+    end
+  end
   def create(%Plug.Conn{} = conn, params) when is_map(params) do
     with {:ok, user} <- Users.create(params) do
       conn |> put_status(201) |> render("user.json", user: user)
