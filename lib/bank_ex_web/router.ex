71ab6f590b0a9892aa05cdc00b328a82b49f0f92
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-27 00:50:35 -0300

    Add docs route

--- lib/bank_ex_web/router.ex
+++ lib/bank_ex_web/router.ex
@@ -12,4 +12,23 @@ defmodule BankExWeb.Router do
     post "/users", UsersController, :create
     get "/users/:referral_code/indications", UsersController, :indications
   end
+
+  scope "/docs" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI,
+      otp_app: :bank_ex,
+      swagger_file: "swagger.json"
+  end
+
+  def swagger_info do
+    %{
+      info: %{
+        version: "1.0.0",
+        title: "BankEx ;)",
+        description: "The new bank account creation method"
+      },
+      tags: [
+        %{name: "Users", description: "Manage all BankEx Users"}
+      ]
+    }
+  end
 end
