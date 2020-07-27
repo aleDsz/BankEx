b5717a1704b90928cf4f044ed8019dc84a2136a6
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-26 23:46:10 -0300

    Add service to handle user indications business logics

---
+++ lib/bank_ex/services/user_indications.ex
@@ -0,0 +1,36 @@
+defmodule BankEx.Services.UserIndications do
+  @moduledoc """
+  The UserIndication service context to handle business logic
+  """
+
+  alias BankEx.Schemas.UserIndication
+  alias BankEx.Contexts.UserIndications
+
+  @doc """
+  Retrieve user indications from referral code 
+  """
+  @spec retrieve_all_from_user_referral_code(referral_code :: binary()) :: {:ok, list(UserIndication.t())} | {:error, term()}
+  def retrieve_all_from_user_referral_code(referral_code) do
+    case UserIndications.retrieve_all_from_user_referral_code(referral_code) do
+      {:ok, user_indications} ->
+        {:ok, user_indications}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Create new UserIndication
+  """
+  @spec create(map()) :: {:ok, UserIndication.t()} | {:error, term()}
+  def create(params) do
+    case UserIndications.create(params) do
+      {:ok, user_indication} ->
+        {:ok, user_indication}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+end
