491dda871d1724dab369c7803d6812ffb15074c9
Alexandre de Souza <alexandre@aledsz.com.br>
2020-07-26 22:53:38 -0300

    Add docs to view

--- lib/bank_ex_web/views/users_view.ex
+++ lib/bank_ex_web/views/users_view.ex
@@ -1,11 +1,11 @@
 defmodule BankExWeb.UsersView do
   @moduledoc """
-  
+  The user view context.
   """
   use BankExWeb, :view
 
   @doc """
-  
+  Render the referral code when creating new user
   """
   def render("user.json", %{user: %{referral_code: referral_code}}) do
     %{message: "User created successfully", referral_code: referral_code}
